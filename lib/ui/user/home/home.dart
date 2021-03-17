1909d05dbbd8b7939df1b28861a5b5be0f2d2670
Kassio Holanda <holandakassio@hotmail.com>
2021-03-16 22:37:22 -0300

    page home user

--- lib/ui/user/home/home.dart
+++ lib/ui/user/home/home.dart
@@ -1,9 +1,13 @@
+import 'package:egresso_ifpi/controllers/login_controller.dart';
+import 'package:egresso_ifpi/ui/config/drawer.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: DrawerConfig(),
       appBar: AppBar(
         title: Text('Pagina Inicial'),
       ),
