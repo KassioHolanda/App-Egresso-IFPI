e13327128b0592cb217b696a6f1a94b7465b2c5d
Kassio Holanda <holandakassio@hotmail.com>
2021-03-16 21:58:08 -0300

    config firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:egresso_ifpi/ui/app/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  GetIt getIt = GetIt.I;
+  // getIt.registerLazySingleton(() => RegisterController());
+
   runApp(App());
 }
