ad69a9feb336b4f891ffb00e3a5052d6be570c1c
Kassio Holanda <holandakassio@hotmail.com>
2021-03-21 16:33:41 -0300

    creating page detail student

---
+++ lib/ui/student/detail_student_screen.dart
@@ -0,0 +1,197 @@
+import 'package:egresso_ifpi/domain/model/student.dart';
+import 'package:egresso_ifpi/utils/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+
+class DetailStudent extends StatelessWidget {
+  final StudentModel student;
+
+  final _formKey = GlobalKey<FormState>();
+  final _scafoldkey = GlobalKey<ScaffoldState>();
+
+  final utils = Utils();
+
+  DetailStudent(this.student);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget formLabel(String title, TextEditingController controller,
+        Function onChanged, Function validade) {
+      return TextFormField(
+        validator: validade,
+        controller: controller,
+        decoration:
+            InputDecoration(labelText: title, border: OutlineInputBorder()),
+      );
+    }
+
+    showMessage(String message) {
+      _scafoldkey.currentState.showSnackBar(SnackBar(
+        content: Text(
+          message,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+        ),
+        behavior: SnackBarBehavior.floating,
+        duration: new Duration(seconds: 3),
+      ));
+    }
+
+    void showCoursesCreated() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return SingleChildScrollView(
+                child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.search),
+                        suffixIcon: Icon(Icons.add)),
+                  ),
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: ScrollPhysics(),
+                  scrollDirection: Axis.vertical,
+                  itemBuilder: (_, index) {
+                    if (utils.coursesFinded.length > 0) {
+                      return ListTile(
+                        trailing: Checkbox(
+                          onChanged: (bool value) {
+                            student.setCourse(utils.coursesFinded[index].uid);
+                          },
+                          value:
+                              utils.coursesFinded[index].uid == student.cursoUid
+                                  ? true
+                                  : false,
+                        ),
+                        leading: Text('${index + 1}'),
+                        title: Text('${utils.coursesFinded[index].descricao}'),
+                        subtitle: Text('${utils.coursesFinded[index].nivel}'),
+                      );
+                    }
+                    return Container();
+                  },
+                  itemCount: utils.coursesFinded.length,
+                )
+              ],
+            ));
+          });
+    }
+
+    return Scaffold(
+      key: _scafoldkey,
+      appBar: AppBar(
+        title: Text('Detalhar aluno'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              formLabel('Nome', TextEditingController(text: student.nome),
+                  student.setNome, (String text) {
+                if (text.isEmpty) {
+                  return 'Campo obrigatório';
+                }
+              }),
+              SizedBox(
+                height: 10,
+              ),
+              formLabel('Email', TextEditingController(text: student.email),
+                  student.setEmail, (String text) {
+                if (text.isEmpty) {
+                  return 'Campo obrigatório';
+                } else if (!text.contains('@')) {
+                  return 'Email inválido.';
+                }
+              }),
+              SizedBox(
+                height: 10,
+              ),
+              formLabel(
+                  'Matricula',
+                  TextEditingController(text: student.matricula.toUpperCase()),
+                  student.setMatricula, (String text) {
+                if (text.isEmpty) {
+                  return 'Campo obrigatório';
+                }
+              }),
+              SizedBox(
+                height: 10,
+              ),
+              formLabel(
+                  'Data nascimento',
+                  TextEditingController(
+                      text: utils.returnDateDefault(student.dataNascimento)),
+                  student.setMatricula, (String text) {
+                if (text.isEmpty) {
+                  return 'Campo obrigatório';
+                }
+              }),
+              SizedBox(
+                height: 10,
+              ),
+              formLabel(
+                  'Status matricula',
+                  TextEditingController(
+                      text: student.status.toUpperCase().replaceAll('_', ' ')),
+                  student.setMatricula, (String text) {
+                if (text.isEmpty) {
+                  return 'Campo obrigatório';
+                }
+              }),
+              SizedBox(
+                height: 10,
+              ),
+              formLabel(
+                  'CPF',
+                  MaskedTextController(
+                      mask: '000.000.000-00', text: student.cpf),
+                  student.setMatricula, (String text) {
+                if (text.isEmpty) {
+                  return 'Campo obrigatório';
+                }
+              }),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                readOnly: true,
+                onTap: () {
+                  showCoursesCreated();
+                },
+                decoration: InputDecoration(
+                    suffixIcon: Icon(Icons.edit),
+                    labelText: 'Curso',
+                    // enabled: false,
+                    border: OutlineInputBorder()),
+              ),
+              SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(6),
+                  color: Colors.red[300],
+                ),
+                width: MediaQuery.of(context).size.width,
+                child: FlatButton(
+                  child: Text(
+                    'Continuar',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {}
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/student/student_screen.dart
@@ -0,0 +1,113 @@
+import 'package:egresso_ifpi/domain/model/student.dart';
+import 'package:egresso_ifpi/ui/config/drawer.dart';
+import 'package:egresso_ifpi/ui/student/detail_student_screen.dart';
+import 'package:egresso_ifpi/utils/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get_it/get_it.dart';
+
+class StudentScreen extends StatelessWidget {
+  final drawerController = GetIt.I.get<Utils>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      drawer: DrawerConfig(),
+      appBar: AppBar(
+        title: Text('Alunos encontrados'),
+        actions: [IconButton(icon: Icon(Icons.search), onPressed: () {})],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: FutureBuilder<QuerySnapshot>(
+          future: FirebaseFirestore.instance.collection('alunos').get(),
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                  child: Container(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              default:
+                final documentos = snapshot.data.docs;
+                if (documentos.length > 0) {
+                  return Container(
+                    child: ListView.builder(
+                      itemBuilder: (_, index) {
+                        StudentModel student =
+                            StudentModel.fromDocument(documentos[index]);
+                        return Card(
+                            child: ExpansionTile(
+                          tilePadding: EdgeInsets.only(right: 10),
+                          expandedAlignment: Alignment.topLeft,
+                          childrenPadding:
+                              EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                          children: [
+                            TextFormField(
+                              enabled: false,
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                              decoration: InputDecoration(
+                                labelText: 'Status matricula:',
+                                border: InputBorder.none,
+                              ),
+                              controller: TextEditingController(
+                                  text:
+                                      '${student.status.replaceAll('_', ' ').toUpperCase()}'),
+                            ),
+                            // TextFormField(
+                            //   enabled: false,
+                            //   decoration: InputDecoration(
+                            //     labelText: 'Data nascimento:',
+                            //   ),
+                            //   controller: TextEditingController(
+                            //       text:
+                            //           '${utils.returnDateDefault(student.dataNascimento)}'),
+                            // ),
+                            // SizedBox(
+                            //   height: 10,
+                            // ),
+                            Container(
+                              width: double.infinity,
+                              child: FlatButton(
+                                  color: Colors.red,
+                                  onPressed: () {
+                                    Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                            builder: (BuildContext context) {
+                                      return DetailStudent(student);
+                                    }));
+                                  },
+                                  child: Text(
+                                    'Detalhar',
+                                    style: TextStyle(color: Colors.white),
+                                  )),
+                            ),
+                          ],
+                          title: ListTile(
+                            leading: CircleAvatar(
+                              child: Icon(Icons.person),
+                            ),
+                            title: Text(
+                              '${student.nome}',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            subtitle:
+                                Text('${student.matricula.toUpperCase()}'),
+                          ),
+                        ));
+                      },
+                      itemCount: documentos.length,
+                    ),
+                  );
+                }
+                return Container();
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
